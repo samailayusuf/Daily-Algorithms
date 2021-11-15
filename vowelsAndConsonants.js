7ec562e33bd633e5b37e35916b133a6486f2f69c
unknown <yusuf7samaila@gmail.com>
2021-11-15 09:36:50 +0100

    check vowels and consonants

---
+++ vowelsAndConsonants.js
@@ -0,0 +1,35 @@
+const check = string => {
+    const text = string.toLowerCase().split('')
+    const vowels = ['a','i', 'o', 'u', 'e']
+
+    //console.log(text)
+
+    let teamVowels = 0;
+    let teamConsonants = 0;
+    let verdict = "";
+
+
+    for (let i=0; i<text.length; i++){
+        if (vowels.includes(text[i])){
+            teamVowels++
+        }else{
+            teamConsonants++
+        }
+    }
+
+    if(teamConsonants > teamVowels){
+        verdict = "consonants win"
+    }else if(teamVowels > teamConsonants){
+        verdict = "vowels win"
+    }else{
+        verdict = "draw"
+    }
+
+    return verdict
+
+
+}
+
+let res = check('EEEEEEemployee')
+
+console.log(res)
