d0286d69e790dc234670137d0e8d5ebc4fcfa234
unknown <yusuf7samaila@gmail.com>
2021-11-15 09:08:53 +0100

    corrected so that function returns duplicate only once

--- nonUnique.js
+++ nonUnique.js
@@ -5,11 +5,13 @@ const findNonUnique = array => {
 
     for(let i=0; i<sortedArray.length; i++){
         if (sortedArray[i] === sortedArray[i+1]){
-            duplicates.push(sortedArray[i])
+            if(! duplicates.includes(sortedArray[i])){
+                duplicates.push(sortedArray[i])
+            }
         }
     }
 
-    return duplicates
+    return (duplicates)
 }
 
 const test = findNonUnique([1,2,2,2,3,4,5,6,7,8,9,0,0,0])
