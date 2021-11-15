c6d867c785f3b17438ea11be15fab5df58d7bedf
unknown <yusuf7samaila@gmail.com>
2021-11-15 08:57:15 +0100

    nonUnique done

---
+++ nonUnique.js
@@ -0,0 +1,17 @@
+const findNonUnique = array => {
+    const sortedArray = array.slice().sort()
+
+    let duplicates = []
+
+    for(let i=0; i<sortedArray.length; i++){
+        if (sortedArray[i] === sortedArray[i+1]){
+            duplicates.push(sortedArray[i])
+        }
+    }
+
+    return duplicates
+}
+
+const test = findNonUnique([1,2,2,2,3,4,5,6,7,8,9,0,0,0])
+
+console.log(test) // [ 0, 0, 2, 2 ]
