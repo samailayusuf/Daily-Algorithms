dc722fd0e8a6ab1b9635e8294b98a701fa5e2a39
Yusuf Samaila <yusuf7samaila@gmail.com>
2021-12-14 21:00:37 +0100

    complementary DNA

---
+++ dna.js
@@ -0,0 +1,22 @@
+function DNAStrand(dna){
+    //your code here
+    var dnaStr = dna.split('')
+    console.log(dnaStr)
+    
+    
+    for (let i=0; i<dnaStr.length; i++){
+      if(dnaStr[i] === "A"){
+        dnaStr[i] = "T"
+      }else if(dna[i] === "T"){
+        dnaStr[i] = "A"
+      }else if (dna[i] === "C"){
+        dnaStr[i]="G"
+      }else if(dna[i] === "G"){
+        dnaStr[i]="C"
+      }
+      
+    }
+    return dnaStr.join('');
+  }
+
+  console.log(DNAStrand('GTAT'))
