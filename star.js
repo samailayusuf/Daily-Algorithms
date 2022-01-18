d235588498a5d217e2fdb2527a8fccf54a23628d
Yusuf Samaila <yusuf7samaila@gmail.com>
2022-01-18 08:27:47 +0100

    star

--- star.js
+++ star.js
@@ -1,13 +1,8 @@
-const check = (value) =>{
+const rate = (value) =>{
     [1,2,3,4,5].map((rate)=>{
-        value +1 === rate + 0.5 ? console.log('🌓') : value >= rate ? console.log('🌕') : console.log('📪')
+        value +1 === rate + 0.5 ? console.log('Half') : value >= rate ? console.log('Full') : console.log('Empty')
     })
 }
 
-check(2.5)
+rate(2.5)
 
-// if(value+1 ===rate +0.5){
-//     console.log('half')
-// }else if( value >= rate){
-    
-// }
